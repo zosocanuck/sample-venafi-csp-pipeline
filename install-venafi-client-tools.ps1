0516c3a016114746bf6444c17f67bed025e3a793
Ivan Wallis <iwallis@gmail.com>
2023-09-01 11:14:12 -0700

    Create install-venafi-client-tools.ps1

---
+++ install-venafi-client-tools.ps1
@@ -0,0 +1,12 @@
+$ErrorActionPreference = 'Stop'
+Set-PSDebug -Trace 1
+
+$proc = Start-Process -Wait -NoNewWindow -PassThru msiexec `
+    -ArgumentList ('/i','\build\client.msi', `
+    '/passive','/l*vx','\build\venafi-install.log')
+if ($proc.ExitCode -ne 0) {
+    Get-Content \build\venafi-install.log
+    exit 1
+}
+
+setx /M PATH "${env:PATH};${env:ProgramFiles}\Venafi CodeSign Protect"
